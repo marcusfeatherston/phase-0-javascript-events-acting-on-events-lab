30d90525e886bfa10343ca0662116f27f55e00f8
marcus featherston <marcusovfeatherston@gmail.com>
2023-02-02 10:41:21 -0700

    setup eventlistener for left with functionality, didnt bother with right.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#FF69B4";
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+const moveDodgerLeft = () => {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+};
